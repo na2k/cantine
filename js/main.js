e34b14d94abd28ac8ed4f65d18b3b6411700307d
Aninot <43991754+Aninot@users.noreply.github.com>
2018-10-10 16:36:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$('#enable').hide();
+$('#disable').hide();
+window.onload=function()
+{
+  horloge();
+}
+function horloge() {
+  function actualiser() {
+    var date = new Date();
+    if(!((date.getHours == 10 && date.getMinutes<30) || date.getHours < 10))
+    {
+      $('#enable').slideUp('slow');
+      $('#disable').slideDown('slow');
+    }
+
+  }
+  actualiser();
+  setInterval(actualiser,1000);
+}
 
