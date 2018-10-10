65a4d50480100837b358f158242c71eb17a30e40
Aninot <43991754+Aninot@users.noreply.github.com>
2018-10-10 21:59:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $('#enable').hide();
 $('#disable').hide();
+$('#all').hide();
+$('#cantiniere').show();
+$('#empty').hide();
+let plats =  [];
+
 window.onload=function()
 {
   horloge();
@@ -18,3 +23,41 @@ function horloge() {
   setInterval(actualiser,1000);
 }
 
+function showMenu(email)
+{
+    if(email=="cantiniere")
+    {
+      $('#all').hide();
+      $('#cantiniere').show();
+    }
+}
+
+function showDish()
+{
+    $('#listeMenuAdmin').empty();
+    for(let i = 0;i < plats.length; i++)
+    {
+        $('#listeMenuAdmin').append('<li class="list-group-item d-flex justify-content-between plat">'+ plats[i] +'<button class="delete btn btn-dark btn-xs" data-target="'+i+'">X</button></li>');
+    }
+}
+
+$('#addDish').click(function(){
+    let plat = $('#plat').val();
+    if(plat.length>0)
+    {
+        plats.push(plat);
+        $('#empty').slideUp('slow');
+        $('#plat').val('');
+        showDish();
+    }
+    else
+    {
+        $('#empty').slideDown('slow');
+    }
+})
+
+$("#listeMenuAdmin").on('click', '.delete', function () {
+    $(this).parent().remove();
+    plats.splice($(this).data('listeMenuAdmin'),1)
+    showDish();
+});
